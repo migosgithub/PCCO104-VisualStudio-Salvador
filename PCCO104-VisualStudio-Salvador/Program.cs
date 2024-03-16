abb02dcff0ebe11e90096d24e78d17b9e793e2d4
Jannah Claravall <claravall@example.com>
2024-03-16 11:12:05 +0800

    Add project files.

---
+++ PCCO104-VisualStudio-Salvador/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PCCO104_VisualStudio_Salvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+
+            string[] names = { "Cyrus", "Sir Ralph"};
+            {
+                Console.WriteLine($"[name]");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
